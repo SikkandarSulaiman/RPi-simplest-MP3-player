7991d8018f3a38927dee1807a263956a872fcfab
SikkandarSulaiman <sikkandarsulaiman@gmail.com>
2016-08-24 15:27:09 +0530

    Add files via upload

---
+++ playMP3.py
@@ -0,0 +1,12 @@
+import os, random
+
+path = '/home/pi/Music/'
+mp3files = []
+
+for f in os.listdir(path):
+    if f[len(f)-4:]=='.mp3':
+        mp3files.append(f)
+
+
+while True:
+    os.system('omxplayer '+path+random.choice(mp3files))
